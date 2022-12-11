7e078be8b1e3d90d41e15c421d0a7b6f721e40bb
AmeerAllawati <amaab@ucdavis.com>
2022-12-11 12:35:11 +0400

    Flexible symbols has been added

--- src/tic_tac_toe_game.java
+++ src/tic_tac_toe_game.java
@@ -7,11 +7,13 @@ public class tic_tac_toe_game {
 	static public char[][] board = {{'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'}};
 	static public int player = 1;
 	static public int place;
+	static public char player1_symbol;
+	static public char player2_symbol;
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		
+		symbol_choosing();
 		while (!game_ended()) {
 		print_board();
 		place = ask_move(player);
@@ -21,26 +23,43 @@ public class tic_tac_toe_game {
 		
 	}
 	
+	private static void symbol_choosing() {
+		Scanner s = new Scanner(System.in);
+		do {
+			System.out.println("Game started. Player 1 enter your symbol: ");
+			player1_symbol = s.next().charAt(0);
+			//s.nextLine();
+			System.out.println("Player 2 enter your symbol: ");
+			player2_symbol = s.next().charAt(0);
+			//s.nextLine();
+			if (player1_symbol == player2_symbol) {
+				System.out.println("Not valid, re-enter your symbols");
+			}
+		} while (player1_symbol == player2_symbol);
+
+		
+	}
+
 	private static boolean game_ended() {
 		// TODO Auto-generated method stub
 		
-		boolean player1_win = board[0][0] == board[0][1] && board[0][1] == board[0][2] && board[0][0] == 'X' || //Horizontal 
-				board[1][0] == board[1][1] && board[1][1] == board[1][2] && board[1][0] == 'X' || 
-				board[2][0] == board[2][1] && board[2][1] == board[2][2] && board[2][0] == 'X'||
-				board[0][0] == board[1][0] && board[1][0] == board[2][0] && board[0][0] == 'X'|| //Vertical
-				board[0][1] == board[1][1] && board[1][1] == board[2][1] && board[0][1] == 'X'|| 
-				board[0][2] == board[1][2] && board[1][2] == board[2][2] && board[0][2] == 'X'||
-				board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] == 'X'|| //Diagonals
-				board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] == 'X';
+		boolean player1_win = board[0][0] == board[0][1] && board[0][1] == board[0][2] && board[0][0] == player1_symbol || //Horizontal 
+				board[1][0] == board[1][1] && board[1][1] == board[1][2] && board[1][0] == player1_symbol || 
+				board[2][0] == board[2][1] && board[2][1] == board[2][2] && board[2][0] == player1_symbol||
+				board[0][0] == board[1][0] && board[1][0] == board[2][0] && board[0][0] == player1_symbol|| //Vertical
+				board[0][1] == board[1][1] && board[1][1] == board[2][1] && board[0][1] == player1_symbol|| 
+				board[0][2] == board[1][2] && board[1][2] == board[2][2] && board[0][2] == player1_symbol||
+				board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] == player1_symbol|| //Diagonals
+				board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] == player1_symbol;
 				
-		boolean player2_win = board[0][0] == board[0][1] && board[0][1] == board[0][2] && board[0][0] == 'O' || //Horizontal 
-				board[1][0] == board[1][1] && board[1][1] == board[1][2] && board[1][0] == 'O' || 
-				board[2][0] == board[2][1] && board[2][1] == board[2][2] && board[2][0] == 'O'||
-				board[0][0] == board[1][0] && board[1][0] == board[2][0] && board[0][0] == 'O'|| //Vertical
-				board[0][1] == board[1][1] && board[1][1] == board[2][1] && board[0][1] == 'O'|| 
-				board[0][2] == board[1][2] && board[1][2] == board[2][2] && board[0][2] == 'O'||
-				board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] == 'O'||
-				board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] == 'O';
+		boolean player2_win = board[0][0] == board[0][1] && board[0][1] == board[0][2] && board[0][0] == player2_symbol || //Horizontal 
+				board[1][0] == board[1][1] && board[1][1] == board[1][2] && board[1][0] == player2_symbol || 
+				board[2][0] == board[2][1] && board[2][1] == board[2][2] && board[2][0] == player2_symbol||
+				board[0][0] == board[1][0] && board[1][0] == board[2][0] && board[0][0] == player2_symbol|| //Vertical
+				board[0][1] == board[1][1] && board[1][1] == board[2][1] && board[0][1] == player2_symbol|| 
+				board[0][2] == board[1][2] && board[1][2] == board[2][2] && board[0][2] == player2_symbol||
+				board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] == player2_symbol||
+				board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] == player2_symbol;
 		if (player1_win) {
 			System.out.println("Player 1 won!");
 			return true;
@@ -97,19 +116,19 @@ public class tic_tac_toe_game {
 	static public void add_board_move(int player_turn, int place) {
 		if (player_turn == 1) {
 			if (place == 1 || place == 2 || place == 3) {
-				board[0][place-1] = 'X';
+				board[0][place-1] = player1_symbol;
 			} else if (place == 4 || place == 5 || place == 6) {
-				board[1][place-4] = 'X';
+				board[1][place-4] = player1_symbol;
 			} else if (place == 7 || place == 8 || place == 9) {
-				board[2][place-7] = 'X';
+				board[2][place-7] = player1_symbol;
 			}
 		} else if (player_turn == 2) {
 			if (place == 1 || place == 2 || place == 3) {
-				board[0][place-1] = 'O';
+				board[0][place-1] = player2_symbol;
 			} else if (place == 4 || place == 5 || place == 6) {
-				board[1][place-4] = 'O';
+				board[1][place-4] = player2_symbol;
 			} else if (place == 7 || place == 8 || place == 9) {
-				board[2][place-7] = 'O';
+				board[2][place-7] = player2_symbol;
 			}
 		}
 	}
@@ -130,7 +149,11 @@ public class tic_tac_toe_game {
 
 	private static boolean valid_move(int move) {
 		// TODO Auto-generated method stub
-		if (move == 1 || move == 2 || move == 3 || move == 4 || move == 5 || move == 6 || move == 7 || move == 8 || move == 9) {
+		if (move == 1 && (board[0][0] != player1_symbol || board[0][0] != player2_symbol) || move == 2 && (board[0][1] != player1_symbol || board[0][1] != player2_symbol) ||
+			move == 3 && (board[0][2] != player1_symbol || board[0][2] != player2_symbol) || move == 4 && (board[1][0] != player1_symbol || board[1][0] != player2_symbol) || 
+			move == 5 && (board[1][1] != player1_symbol || board[1][1] != player2_symbol) || move == 6 && (board[1][2] != player1_symbol || board[1][2] != player2_symbol) || 
+			move == 7 && (board[2][0] != player1_symbol || board[2][0] != player2_symbol) || move == 8 && (board[2][1] != player1_symbol || board[2][1] != player2_symbol) || 
+			move == 9 && (board[2][2] != player1_symbol || board[2][2] != player2_symbol) ) {
 			return true;
 		} else {
 			System.out.println("Not a valid move!");
